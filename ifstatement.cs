39c0d32e720b881bf45b7fb3c96c546c044bdeea
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-05-01 04:13:07 +0300

    Create ifstatement.cs

---
+++ ifstatement.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 50;
+            int sayi3 = 60;
+
+            if (sayi1 == 10 && sayi1 == sayi2 && sayi1 == sayi3)
+            {
+                Console.WriteLine("Hepsi Birbirine Eşittir");
+            }
+
+            else
+            {
+                Console.WriteLine("Degerler Eşit Degildir");
+            }
+
+            Console.ReadLine();
+        }
+            }
+        }

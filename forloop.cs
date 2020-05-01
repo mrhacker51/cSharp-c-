d13119880c192e2bf27e12e0fa4abf5ba7e1769f
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-05-02 00:34:57 +0300

    Update forloop.cs

--- forloop.cs
+++ forloop.cs
@@ -23,28 +23,3 @@ namespace forloop
         }
     }
 }
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace forloop
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //for (int i = 0; i <= 100; i++)
-            //for (int i = 1; i <= 100; i += 2)
-            //for (int i = 100; i >= 0; i-=2)
-            {
-                Console.WriteLine(i);
-            }
-
-            Console.WriteLine("Finished!!!");
-
-            Console.ReadLine();
-        }
-    }
-}

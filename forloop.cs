61250d89942ad0606cb9d632774b89dcded59cfe
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-05-02 00:34:28 +0300

    Create forloop.cs

---
+++ forloop.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i <= 100; i++)
+            //for (int i = 1; i <= 100; i += 2)
+            //for (int i = 100; i >= 0; i-=2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Finished!!!");
+
+            Console.ReadLine();
+        }
+    }
+}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i <= 100; i++)
+            //for (int i = 1; i <= 100; i += 2)
+            //for (int i = 100; i >= 0; i-=2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Finished!!!");
+
+            Console.ReadLine();
+        }
+    }
+}

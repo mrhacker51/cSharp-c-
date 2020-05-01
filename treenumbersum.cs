f0b4f0aefbb6f3d10e723eac01d6547f10ce4710
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-05-01 04:06:08 +0300

    Create treenumbersum.cs

---
+++ treenumbersum.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 20;
+            int sayi3 = 30;
+            int toplam = sayi1 + sayi2 + sayi3;
+
+            Console.WriteLine(toplam);
+            Console.ReadLine();
+        }
+            }
+        }
